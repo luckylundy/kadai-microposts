92d724adc3b31bb54eca19e5eb02f6be50ce39a8
toyotaka.kamon <toyotaka-kamon@outlook.jp>
2020-11-23 12:22:42 +0000

    post

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,5 +1,9 @@
 class ToppagesController < ApplicationController
   def index
     # アクションの最後にデフォルトでrender :indexを呼びだし
+    if logged_in?
+      @micropost = current_user.microposts.build  # form_with 用
+      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+    end
   end
 end
